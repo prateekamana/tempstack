22212fe6ffbc42aa0b8555ea007bc9df20f97188
prateekamana <prateekamana@gmail.com>
2020-05-24 20:21:59 +0530

    update

--- mysite/routing.py
+++ mysite/routing.py
@@ -1,5 +1,14 @@
-from channels.routing import ProtocolTypeRouter
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from articles.api import consumers
 
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
+    'websocket': AllowedHostsOriginValidator(
+    	URLRouter([
+    		path('api/', consumers.ArticleConsumer)
+
+    		])
+    )
 })
