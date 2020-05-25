f3b579130a6bdb25198bf7b2435bcaa4b2d3c7ff
prateekamana <prateekamana@gmail.com>
2020-05-25 16:44:05 +0530

    changes 1

--- articles/api/consumers.py
+++ articles/api/consumers.py
@@ -8,9 +8,20 @@ from channels.db import database_sync_to_async
 class ArticleConsumer(AsyncConsumer):
 	async def websocket_connect(self, event):
 		print('ws connected', event)
+		await self.send({
+            "type": "websocket.accept",
+        })
+
 
 	async def websocket_receive(self, event):
 		print('ws receive', event)
+		await self.send({
+            "type": "websocket.send",
+            "text": event["text"],
+        })
 
 	async def websocket_disconnect(self, event):
 		print('ws disconnected', event)
+		await self.send({
+            "type": "websocket.close",
+        })
